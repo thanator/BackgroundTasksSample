49e6b1642f34543aa0fc47f40bdbc7363754055d
thanator <www.godx@mail.ru>
2017-05-18 20:14:33 +0300

    all

--- app/src/main/java/ru/sberbank/mobile/common/rate/net/RatesActivity.java
+++ app/src/main/java/ru/sberbank/mobile/common/rate/net/RatesActivity.java
@@ -2,12 +2,17 @@ package ru.sberbank.mobile.common.rate.net;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import ru.sberbank.backgroundtaskssample.R;
+import ru.sberbank.mobile.common.rate.entity.RatesBundle;
 import ru.sberbank.mobile.common.rate.ui.RatesAdapter;
 
 /**
@@ -16,11 +21,18 @@ import ru.sberbank.mobile.common.rate.ui.RatesAdapter;
 
 public class RatesActivity extends AppCompatActivity {
 
+    private final static int LOADER_ID = 123;
+    private final static String TAG = "RatesActivity";
+
     private RecyclerView mRecyclerView;
     private RatesActivity mRatesActivity;
     private RatesAdapter mRatesAdapter;
     private ProgressBar mProgressBar;
     private SwipeRefreshLayout mSwipeRefreshLayout;
+
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,7 +43,34 @@ public class RatesActivity extends AppCompatActivity {
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mRecyclerView.setAdapter(mRatesAdapter);
+        getSupportLoaderManager().initLoader(LOADER_ID, null, new RatesLoaderCallBacks());
+        Log.v(TAG, "onCreate");
 
 
     }
+
+
+
+    private class RatesLoaderCallBacks implements LoaderManager.LoaderCallbacks <RatesBundle>{
+
+
+        @Override
+        public Loader<RatesBundle> onCreateLoader(int id, Bundle args) {
+            return new RatesLoader(RatesActivity.this);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<RatesBundle> loader, RatesBundle data) {
+            mRatesAdapter.setRates(data.getRates());
+            mProgressBar.setVisibility(View.GONE);
+            mSwipeRefreshLayout.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<RatesBundle> loader) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mSwipeRefreshLayout.setVisibility(View.GONE);
+        }
+    }
+
 }
--- app/src/main/java/ru/sberbank/mobile/common/rate/net/RatesLoader.java
+++ app/src/main/java/ru/sberbank/mobile/common/rate/net/RatesLoader.java
@@ -2,6 +2,7 @@ package ru.sberbank.mobile.common.rate.net;
 
 import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
 
 import ru.sberbank.mobile.common.rate.entity.RatesBundle;
 
@@ -11,14 +12,28 @@ import ru.sberbank.mobile.common.rate.entity.RatesBundle;
 
 public class RatesLoader extends AsyncTaskLoader<RatesBundle> {
 
-    
+    private static final String TAG= "RatesLoader";
+    private final ICurrencyRateApiMapper mRatesApi;
+
 
     public RatesLoader(Context context) {
         super(context);
+        mRatesApi = ApiFactory.createApi();
+
     }
 
     @Override
     public RatesBundle loadInBackground() {
-        return null;
+        Log.v(TAG, "on back");
+        return mRatesApi.getRatesBundle();
+    }
+
+
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+        Log.v(TAG, "on start loading");
     }
 }
